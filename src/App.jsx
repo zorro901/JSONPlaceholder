b4ba7c90a9b3084af1f2c51aee01184e503d71da
zorro901 <zoro907@gmail.com>
2021-08-09 02:12:08 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/3")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のusers</button>
+    </div>
+  );
+}
